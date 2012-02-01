ffab2e2b81d156b2ee2e5214bc1e3999ef83db3a
Joseph McCullough <joseph@vertstudios.com>
2012-02-01 12:52:04 -0600

    zone table specs pass

--- lib/db.rb
+++ lib/db.rb
@@ -5,6 +5,7 @@ require 'sqlite3'
 require 'configuration'
 
 class DB
+	attr_reader :db
 
 	def initialize(configFile=nil)
 
@@ -14,17 +15,60 @@ class DB
 			Configuration.new(configFile)
 		end
 
-		# Get the database profile
+		# Get the database profile and db object
 		@config = Configuration.db
+	end
 
-		# Now create a database
+	# Create the database with the proper schema.
+	def create
 		@db = SQLite3::Database.new(@config['location'])
-		rows = @db.execute <<-SQL
-			create table numbers(
-				name varchar(30),
-				val int
+		@db.execute <<-SQL
+			create table zones (
+				id INTEGER PRIMARY KEY,
+				domain TEXT,
+				zone_id TEXT,
+				comment TEXT
 			);
 		SQL
 	end
 
+	# Extract the zone_id from a zone object's host_url property
+	def zone_id(zone)
+		# Example host_url: /hostedzone/zoneID
+		return zone.host_url.split('/')[2]
+	end
+
+	# Create an entry into the zones table. 
+	def add_zone(zone)
+
+		# The domain is the name property
+		domain = "'#{zone.name}'"
+
+		# Get the urlhost from the zone
+		zone_id = "'#{zone_id(zone)}'"
+
+		@db.execute <<-SQL
+			insert into zones (domain, zone_id)
+			values (#{domain},#{zone_id})
+		SQL
+	end
+
+	# Alter the zone name string so that it can be used as a table name
+	def clean_zone_name(zone)
+		zone.name.downcase.gsub(/[^a-z0-9]/, '_')
+	end
+
+	# Create a table for a zone that we will store records in
+	def create_zone_table(zone)
+
+		@db.execute <<-SQL
+			create table #{clean_zone_name(zone)} (
+				id INTEGER PRIMARY KEY,
+				name TEXT,
+				type TEXT,
+				ttl INT
+				vals TEXT
+			);
+		SQL
+	end
 end
--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
+require 'ostruct'
 require './lib/db'
+require './lib/dns'
 
 describe DB do
 	before(:all) do
 		Configuration.new('./spec/.route53_test')
 		@config = Configuration.db
 		@db = DB.new
+
+		# Create pretend zone/record objects. 
+		@zone = OpenStruct.new({
+			:name => "somedomain.com",
+			:host_url => "/hostedzone/LOL123"
+		})
+
+		@record = OpenStruct.new({
+			:name => "www.somedomain.com",
+			:type => "CNAME",
+			:ttl => 900,
+			:values => ["ns-1741.awsdns-25.co.uk.", 
+									"ns-1092.awsdns-08.org.", 
+									"ns-950.awsdns-54.net.", 
+									"ns-331.awsdns-41.com."] 
+		})
 	end
 
 	# Delete the database after the test if the database was 
@@ -16,15 +34,57 @@ describe DB do
 		end
 	end
 
-	describe "initilaize" do
-		it "should return a DB object" do
-			@db.should_not be_nil
+	describe "#create" do
+		it "should create a database at the path specified in the config file" do
+			@db.create
+			File.exists?(@config['location']).should be_true
 		end
 	end
 
-	describe "#create" do
-		it "should create a database at the path specified in the config file" do
-			File.exists?(@config['location']).should be_true
+	describe "#zone_id" do
+		it "should return the zone's zone id" do
+			@db.zone_id(@zone).should eq("LOL123")
+		end
+
+	end
+
+	describe "#add_zone" do
+		it "should create a zone entry" do
+			@db.add_zone(@zone)
+
+			# Attempt to find the zone added
+			result = @db.db.execute <<-SQL
+				SELECT * from zones
+				WHERE domain='#{@zone.name}' AND
+				zone_id='#{@db.zone_id(@zone)}'
+			SQL
+			result.should_not be_empty
+		end
+	end
+
+	describe "#clean_zone_name" do
+		it "should replace all non-alphanumeric characters with underscores" do
+			@db.clean_zone_name(@zone).should eq("somedomain_com")
+		end
+	end
+
+	describe "#create_zone_table" do
+		it "should create a table for the specified zone" do
+			@db.create_zone_table(@zone)
+
+			# Try to select something from the newly created table.
+			tableExists=true
+			begin
+				@db.db.execute <<-SQL
+					select * from #{@db.clean_zone_name(@zone)}
+				SQL
+			rescue SQLite3::SQLException => e
+				if e.message =~ /no such table/i
+					tableExists=false
+				end
+			end
+
+			tableExists.should be_true
 		end
 	end
 end
