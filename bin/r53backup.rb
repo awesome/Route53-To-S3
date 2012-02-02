12e428934d7527c38d11b03554c0ddb5de4ff60f
Joseph McCullough <joseph@vertstudios.com>
2012-02-02 13:50:47 -0600

    Daemon start works, don't know about background

--- bin/r53backup.rb
+++ bin/r53backup.rb
@@ -6,15 +6,52 @@ $:.unshift File.join(File.dirname(__FILE__), "../lib")
 require 'bundler/setup'
 require 'rubygems'
 require 'route53backup'
+require 'daemon'
+require 'timer'
 
-# Create configuration object from the config file
-Configuration.new('spec/.route53_test')
+class Route53ToS3 < Daemon::Base
+	def self.start
+		# Create configuration object from the config file
+		Configuration.new('spec/.route53_test')
 
-# Connect to S3
-Route53Backup.connect
+		# Do an initial upload on start
+		upload
 
-# Grab DNS records and populate a sqlite3 database with them
-Route53Backup.populate_db
+		# Now upload based on time if specified in the configuration file.
+		if !Configuration.daemon['times'].nil?
+			timeList = Configuration.daemon['times'].split(',')
+			@timer = Timer.new(timeList)
 
-# Upload the populated db to S3
-Route53Backup.upload_db
+			# Keep looping and waiting until the time has come to upload,
+			# and then upload once it's time!
+			loop do
+
+				if @timer.ready_for_upload? 
+					upload
+				end
+
+				# Sleep for 20 seconds at a time, no need to check as fast as possible!
+				sleep(20)
+			end
+		end
+	end
+
+	def self.stop
+		puts "Stopping Route53 to S3 backups"
+	end
+	
+	private
+	def self.upload
+		# Connect to S3
+		Route53Backup.connect
+
+		# Grab DNS records and populate a sqlite3 database with them
+		Route53Backup.populate_db
+
+		# Upload the populated db to S3
+		Route53Backup.upload_db
+	end
+end
+
+#Route53ToS3.daemonize
+Route53ToS3.start
